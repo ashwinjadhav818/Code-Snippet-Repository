7062b91e375718f7d131e39afc218d6ac3dd69db
AshAll-Rounder <ajcuber818@gmail.com>
2021-06-20 14:58:17 +0530

    Overflow In The Code Preview Solved

--- components/Snippet.js
+++ components/Snippet.js
@@ -24,7 +24,7 @@ export default function Snippet({ snippet, snippetDeleted }) {
 		<div className="bg-gray-100 p-4 rounded-md my-2 shadow-lg">
 			<div className="flex items-center justify-between mb-2">
 				<h2 className="text-xl text-gray-800 font-bold">{snippet.data.name}</h2>
-				<span className="font-bold text-xs text-red-800 px-2 py-1 rounded-lg ">
+				<span className="font-bold text-xs text-red-800 px-2 py-1 rounded-lg">
 					{snippet.data.language}
 				</span>
 			</div>
